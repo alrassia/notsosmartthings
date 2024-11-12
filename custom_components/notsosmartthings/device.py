aabca32795e5464934a70ff65afb25b2d0ffe2d6
Alrassia <haico@vanroeden.info>
2024-11-12 11:38:00 +0100

    Enhance disabled_components method to log disabled components and ensure safe access to attributes

--- custom_components/notsosmartthings/device.py
+++ custom_components/notsosmartthings/device.py
@@ -1,6 +1,8 @@
 from pysmartthings.device import DeviceEntity as OriginalDeviceEntity
 from typing import List
 
+from custom_components.notsosmartthings import _LOGGER
+
 
 
 class DeviceEntity(OriginalDeviceEntity):
@@ -8,5 +10,8 @@ class DeviceEntity(OriginalDeviceEntity):
     def disabled_components(self) -> List[str]:
         """Get the list of disabled components for this device."""
         if hasattr(self._status, '_attributes') and self._status._attributes.get("disabledComponents"):
-            return self._status._attributes["disabledComponents"].value
+            disabled_components = self._status._attributes["disabledComponents"].value
+            if disabled_components is not None:
+                _LOGGER.debug("Device %s has disabled components: %s", self.device_id, disabled_components)
+                return disabled_components
         return []
