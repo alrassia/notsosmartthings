a34bcd979fce4483a3c767eb2d2773456b05da4b
Alrassia <haico@vanroeden.info>
2024-11-12 11:49:26 +0100

    On branch main

--- custom_components/notsosmartthings/device.py
+++ custom_components/notsosmartthings/device.py
@@ -8,7 +8,7 @@ from typing import List
 class DeviceEntity(OriginalDeviceEntity):
     @property
     def disabled_components(self) -> List[str]:
-        """Get the list of disabled components for this device."""
+        """Get the list of disabled components for this device.."""
         if hasattr(self._status, '_attributes') and self._status._attributes.get("disabledComponents"):
             disabled_components = self._status._attributes["disabledComponents"].value
             if disabled_components is not None:
